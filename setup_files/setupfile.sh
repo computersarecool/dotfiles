a470cb0dedd7a542165499cbc7e7f71ac716e280
Willy Nolan <computersarecool@users.noreply.github.com>
2017-03-16 22:23:47 -0700

    Straightens out order of apt-get installs

--- setup_files/setupfile.sh
+++ setup_files/setupfile.sh
@@ -11,11 +11,29 @@ apt-get install -y git
 apt-get install -y mosh
 apt-get install -y vlock
 apt-get install -y emacs
+apt-get install -y mongodb-org
 
 # Upgrade and install python packages
 pip3 install --upgrade pip
 pip3 install virtualenv
 pip3 install pylint
+
+# Configure installing for latest version of node
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+apt-get install -y nodejs
+
+# Make a new folder for npm globals
+mkdir -p "$USERDIR/.npm-global"
+npm config set prefix "$USERDIR/.npm-global"
+export PATH="$USERDIR/.npm-global:$PATH"
+
+# Install npm packages (many of these are emacs add-ons)
+npm install -g tern
+npm install -g standard
+npm install -g jshint
+npm install -g jsonlint
+npm install -g nodemon
+npm install -g gulp
 
 # Make optonox owner of everything in user directory
 chown -R optonox:optonox $USERDIR
@@ -41,7 +59,7 @@ do
         ln -s $f "$USERDIR/.ssh/$b"
     fi
 
-    # emacs service file is a special case, put it in a special place
+    # Move and enable service files
     if [ $b == "service_files" ]; then
         SERVICEFILES="$USERDIR/documents/gitprojects/dotfiles/service_files"/*
         for sf in $SERVICEFILES
@@ -49,8 +67,8 @@ do
             sfb=$(basename $sf)
             ln $sf "/etc/systemd/system/$sfb"
             systemctl enable $sfb
-        Done
-    Fi
+        done
+    fi
 
     # Skip over the setupfiles_dir
     if [ $b == "setup_files" ]; then
@@ -67,6 +85,7 @@ do
         rm -rf "$USERDIR/$b"
     fi
 
+    # Create links to the userdirectory (where bash looks for them)
     ln -s $f "$USERDIR/$b"
 done
 
@@ -74,25 +93,6 @@ done
 rm -rf "$USERDIR/.emacs.d"
 git clone https://github.com/computersarecool/dotemacs.git "$USERDIR/.emacs.d"
 
-# Return ownership of all files in user directory
-chown -R optonox:optonox $USERDIR
-
-# Configure installing for latest version of node
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-apt-get install -y nodejs
-
-# Make a new folder for npm globals
-mkdir -p "$USERDIR/.npm-global"
-npm config set prefix "$USERDIR/.npm-global"
-export PATH="$USERDIR/.npm-global:$PATH"
-
-# Install npm packages (many of these are emacs add-ons)
-npm install -g tern
-npm install -g standard
-npm install -g jshint
-npm install -g jsonlint
-npm install -g nodemon
-npm install -g gulp
 
 # Make a tern config file in home directory
 echo "{
@@ -102,8 +102,7 @@ echo "{
     }
  }" > "$USERDIR/.tern-config"
 
-
-# Install mongodb
-apt-get install -y mongodb-org
+# Return ownership of all files in user directory
+chown -R optonox:optonox $USERDIR
 
 source "$USERDIR/.profile"
